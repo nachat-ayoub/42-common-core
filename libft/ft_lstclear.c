050f7714ea8b35ba1f1fbb480aacca95ddaa175d
Ayoub Nachat <anachat@e3r4p5.1337.ma>
2024-11-02 10:29:57 +0100

    Auto commit from script

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anachat <anachat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 10:31:18 by anachat           #+#    #+#             */
+/*   Updated: 2024/11/02 10:21:23 by anachat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*tmp;
+
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone((*lst), del);
+		(*lst) = tmp;
+	}
+	*lst = NULL;
+}
